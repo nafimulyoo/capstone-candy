8540de2769a4df7bce5e8ba58b494a9645dd913c
nafimulyoo <rxnafimulyo@gmail.com>
2025-05-13 19:31:36 +0700

    add logo

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -12,7 +12,13 @@ export const metadata: Metadata = {
   title: "Astra Digital - Solusi Digital Terpercaya",
   description:
     "Astra Digital adalah perusahaan layanan digital yang dirintis oleh Grup Astra. Kami ada untuk memberikan ide dan solusi dalam membangun visi, atau tujuan bisnis bagi para entrepreneur maupun perusahaan yang bergerak dalam era digital.",
-    generator: 'v0.dev'
+    generator: 'v0.dev',
+    // favicon: '/favicon.ico',
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
 }
 
 export default function RootLayout({
@@ -22,6 +28,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="id" suppressHydrationWarning>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
           {children}
